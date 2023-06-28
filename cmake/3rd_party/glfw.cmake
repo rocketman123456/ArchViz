5801263130ff4afe0133d76affce0ff132768b6b
rocketman123456 <759094438@qq.com>
2023-06-28 11:31:35 +0800

    update

--- cmake/3rd_party/glfw.cmake
+++ cmake/3rd_party/glfw.cmake
@@ -1,7 +1,13 @@
-option(BUILD_SHARED_LIBS "Build shared libraries" OFF)
-option(GLFW_BUILD_EXAMPLES "Build the GLFW example programs" OFF)
-option(GLFW_BUILD_TESTS "Build the GLFW test programs" OFF)
-option(GLFW_BUILD_DOCS "Build the GLFW documentation" OFF)
-option(GLFW_INSTALL "Generate installation target" OFF)
+# option(BUILD_SHARED_LIBS "Build shared libraries" OFF)
+# option(GLFW_BUILD_EXAMPLES "Build the GLFW example programs" OFF)
+# option(GLFW_BUILD_TESTS "Build the GLFW test programs" OFF)
+# option(GLFW_BUILD_DOCS "Build the GLFW documentation" OFF)
+# option(GLFW_INSTALL "Generate installation target" OFF)
+
+SET(BUILD_SHARED_LIBS OFF CACHE BOOL "Build shared libraries" FORCE)
+SET(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "Build the GLFW example programs" FORCE)
+SET(GLFW_BUILD_TESTS OFF CACHE BOOL "Build the GLFW test programs" FORCE)
+SET(GLFW_BUILD_DOCS OFF CACHE BOOL "Build the GLFW documentation" FORCE)
+SET(GLFW_INSTALL OFF CACHE BOOL "Generate installation target" FORCE)
 
 add_subdirectory(glfw)
--- cmake/3rd_party/libzip.cmake
+++ cmake/3rd_party/libzip.cmake
@@ -1,7 +1,13 @@
-option(BUILD_TOOLS "Build tools in the src directory (zipcmp, zipmerge, ziptool)" OFF)
-option(BUILD_REGRESS "Build regression tests" OFF)
-option(BUILD_EXAMPLES "Build examples" OFF)
-option(BUILD_DOC "Build documentation" OFF)
-option(BUILD_SHARED_LIBS "Build shared libraries" OFF)
+# option(BUILD_TOOLS "Build tools in the src directory (zipcmp, zipmerge, ziptool)" OFF)
+# option(BUILD_REGRESS "Build regression tests" OFF)
+# option(BUILD_EXAMPLES "Build examples" OFF)
+# option(BUILD_DOC "Build documentation" OFF)
+# option(BUILD_SHARED_LIBS "Build shared libraries" OFF)
+
+SET(BUILD_TOOLS OFF CACHE BOOL "Build tools in the src directory (zipcmp, zipmerge, ziptool)" FORCE)
+SET(BUILD_REGRESS OFF CACHE BOOL "Build regression tests" FORCE)
+SET(BUILD_EXAMPLES OFF CACHE BOOL "Build examples" FORCE)
+SET(BUILD_DOC OFF CACHE BOOL "Build documentation" FORCE)
+SET(BUILD_SHARED_LIBS OFF CACHE BOOL "Build shared libraries" FORCE)
 
 add_subdirectory(libzip)
